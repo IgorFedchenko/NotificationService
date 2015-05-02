ea46ce65505712c306ac4b230f41be974a8cfafe
IgorFedchenko <igor@fedchenko.pro>
2015-05-03 02:00:32 +0300

    All Done

--- notifications/views.py
+++ notifications/views.py
@@ -396,16 +396,21 @@ class DownloadApplication(View):
         self.write_to_log("Building...")
         # build = pexpect.spawn(os.path.join(app_directory, "gradlew") + " assemble%s"%mode,
         #                       cwd=app_directory, env = {"JAVA_HOME": "/bin/java"})
-        build = pexpect.spawn(os.path.join(app_directory, "build.sh") + " " + mode,
-                              cwd=app_directory, env = {"JAVA_HOME": "/home/igor/soft/jdk1.7.0_71/"})
-        if mode == "Release":
-            build.expect(".*Keystore password.*", timeout=120)
-            build.sendline(app.key.keystore_password)
-            build.expect(".*Key password.*")
-            build.sendline(app.key.key_password)
-        build.expect(pexpect.EOF, timeout=120)
-        self.write_to_log(str(build.before) + "\n" + str(build.after))
-        self.write_to_log("Build finished!")
+        # build = pexpect.spawn(os.path.join(app_directory, "build.sh") + " " + mode,
+        #                       cwd=app_directory, env = {"JAVA_HOME": "/home/igor/soft/jdk1.7.0_71/"},
+        #                       )
+        # if mode == "Release":
+        #     build.expect(".*Keystore password.*")
+        #     build.sendline(app.key.keystore_password)
+        #     build.expect(".*Key password.*")
+        #     build.sendline(app.key.key_password)
+        # build.expect(pexpect.EOF)
+        # self.write_to_log(str(build.before) + "\n" + str(build.after))
+        # self.write_to_log("Build finished!")
+        out = pexpect.run(os.path.join(app_directory, "build.sh") + " " + mode,
+                          cwd=app_directory, env = {"JAVA_HOME": "/home/igor/soft/jdk1.7.0_71/"}
+                         )
+        self.write_to_log(out)
         return os.path.join(app_directory, "app", "build", "outputs", "apk", "app-%s.apk"%mode.lower())
 
     @method_decorator(login_required)
