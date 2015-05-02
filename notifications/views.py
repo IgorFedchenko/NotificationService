d7a9d0a0f3c1af82091b28b186f1ab173ef20e96
IgorFedchenko <igor@fedchenko.pro>
2015-05-03 01:13:05 +0300

    All Done

--- notifications/views.py
+++ notifications/views.py
@@ -393,9 +393,11 @@ class DownloadApplication(View):
         else:
             mode = "Debug"
 
-        self.write_to_log("Building... Cwd: %s"%app_directoryc)
-        build = pexpect.spawn(os.path.join(app_directory, "gradlew") + " assemble%s"%mode,
-                              cwd=app_directory, env = {"JAVA_HOME": "/bin/java"})
+        self.write_to_log("Building...")
+        # build = pexpect.spawn(os.path.join(app_directory, "gradlew") + " assemble%s"%mode,
+        #                       cwd=app_directory, env = {"JAVA_HOME": "/bin/java"})
+        build = pexpect.spawn(os.path.join(app_directory, "build.sh") + mode,
+                              cwd=app_directory, env = {"JAVA_HOME": "/home/igor/soft/jdk1.7.0_71/"})
         if mode == "Release":
             build.expect(".*Keystore password.*")
             build.sendline(app.key.keystore_password)
